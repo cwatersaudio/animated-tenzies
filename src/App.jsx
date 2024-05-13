7277c718ec3f322892ab160efe85083253d1d693
Chris Waters <48170993+cwatersaudio@users.noreply.github.com>
2024-05-13 09:16:41 -0400

    trying to use innwe/outerWidth

--- src/App.jsx
+++ src/App.jsx
@@ -108,12 +108,14 @@ export default function App() {
     setDice(allNewDice)
   }
 
+  const { innerWidth: width, innerHeight: height } = window;
+
 
   return (
 
     <main>
       {tenzies ?
-        <Confetti />
+        <Confetti width={width} height={height} />
         : null}
       {tenzies ? <h1>You Won Tenzies!</h1> : <h1>Tenzies</h1>}
       <p>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
